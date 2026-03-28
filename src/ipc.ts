fb5d027b59eed88dd6046eac7f22e89e53fbbb81
SilverFire - Dmytro Naumenko <d.naumenko.a@gmail.com>
2026-03-28 19:07:59 +0200

    wip: save changes before update

--- src/ipc.ts
+++ src/ipc.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import { CronExpressionParser } from 'cron-parser';
 
 import { DATA_DIR, IPC_POLL_INTERVAL, TIMEZONE } from './config.js';
+import { initBotPool, sendPoolMessage } from './channels/telegram.js';
+export { initBotPool };
 import { AvailableGroup } from './container-runner.js';
 import { createTask, deleteTask, getTaskById, updateTask } from './db.js';
 import { isValidGroupFolder } from './group-folder.js';
@@ -81,7 +83,11 @@ export function startIpcWatcher(deps: IpcDeps): void {
                   isMain ||
                   (targetGroup && targetGroup.folder === sourceGroup)
                 ) {
-                  await deps.sendMessage(data.chatJid, data.text);
+                  if (data.sender && data.chatJid.startsWith('tg:')) {
+                    await sendPoolMessage(data.chatJid, data.text, data.sender, sourceGroup);
+                  } else {
+                    await deps.sendMessage(data.chatJid, data.text);
+                  }
                   logger.info(
                     { chatJid: data.chatJid, sourceGroup },
                     'IPC message sent',
